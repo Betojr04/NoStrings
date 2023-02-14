d3c87b295edd73d6e6a9c2fbcae31ddbc3093692
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-02-14 23:53:55 +0000

    installed JWT package and imported on app.py, routes.py, and pipfile

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,12 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-
 import hashlib
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
 
 
