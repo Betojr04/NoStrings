5f06fee64d4e749d6346558af94e76aa8e26eb95
michbalkany <askmitchb@gmail.com>
2023-02-16 23:21:27 +0000

    worked on the navbar, burger menu and the right hand menu.

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import preferenceIcon from "../../img/options.png";
 export const Navbar = () => {
   const [showOffCanvas, setShowOffCanvas] = useState(false);
-
+  const [leftBurger, setLeftBurger] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <nav className="navbar bg-light fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Offcanvas navbar
-        </a>
+        <img
+          className="preferenceIcon"
+          src={preferenceIcon}
+          onClick={() => setLeftBurger(!leftBurger)}
+        ></img>
         <button
           onClick={() => setShowOffCanvas(true)}
           className="navbar-toggler bg-light"
@@ -18,7 +21,7 @@ export const Navbar = () => {
           data-bs-target="#offcanvasNavbar"
           aria-controls="offcanvasNavbar"
         >
-          <i class="fa-solid fa-bars"></i>
+          <i className="fa-solid fa-bars"></i>
         </button>
         {showOffCanvas ? (
           <div
@@ -27,23 +30,84 @@ export const Navbar = () => {
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
           >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                Offcanvas
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
+            {/* <div className="offcanvas-header">
+              
+            </div> */}
+
+            <button
+              type="button"
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              My Profile
+            </button>
+
+            <div
+              // className="modal fade"
+              // id="staticBackdrop"
+              // data-bs-backdrop="static"
+              // data-bs-keyboard="false"
+              // tabindex="-1"
+              // aria-labelledby="staticBackdropLabel"
+              // aria-hidden="true"
+              id="staticBackdrop"
+              className="modal hide fade in"
+              data-keyboard="false"
+              data-backdrop="static"
+            >
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                      Modal title
+                    </h1>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">...</div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Cancel
+                    </button>
+                    <button type="button" className="btn btn-primary">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
+
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
-                    Home
+                    Dark Mode
                   </a>
+                  <div
+                    className="form-check form-switch"
+                    onClick={() => setDarkMode(!darkMode)}
+                  >
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      role="switch"
+                      id="flexSwitchCheckChecked"
+                      checked={darkMode}
+                    />
+                    <label
+                      className="form-check-label"
+                      for="flexSwitchCheckChecked"
+                    ></label>
+                  </div>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">
@@ -96,6 +160,112 @@ export const Navbar = () => {
             </div>
           </div>
         ) : null}
+      </div>
+      <div>
+        {leftBurger ? (
+          <div className="leftBugerDiv p-2">
+            <div className="accordion" id="accordionPanelsStayOpenExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseOne"
+                    aria-expanded="true"
+                    aria-controls="panelsStayOpen-collapseOne"
+                  >
+                    Accordion Item #1
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseOne"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingOne"
+                >
+                  <div className="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It
+                    is shown by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseTwo"
+                  >
+                    Accordion Item #2
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseTwo"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingTwo"
+                >
+                  <div className="accordion-body">
+                    <strong>This is the second item's accordion body.</strong>{" "}
+                    It is hidden by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2
+                  className="accordion-header"
+                  id="panelsStayOpen-headingThree"
+                >
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseThree"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseThree"
+                  >
+                    Accordion Item #3
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseThree"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingThree"
+                >
+                  <div className="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It
+                    is hidden by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </nav>
   );
