a0735466bec0da53a875d70afe7c398aa06b25a0
michbalkany <askmitchb@gmail.com>
2023-02-22 22:13:15 +0000

    login button brings user home, and anonymous user button has 18+ verification

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -6,6 +6,12 @@ const Login = () => {
   const [password, setPassword] = useState();
   const [error, setError] = useState("");
   const Navigate = useNavigate();
+  const [ageVerified, setAgeVerified] = useState(false);
+  const navigateLink = () => {
+    setTimeout(() => {
+      Navigate("/home");
+    }, 2000);
+  };
   const handlelogin = (e) => {
     e.preventDefault();
     fetch(
@@ -39,50 +45,153 @@ const Login = () => {
       });
   };
   return (
-    <form className="container mt-5" onSubmit={handlelogin}>
-      <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">
-          Email address
-        </label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" className="form-text">
-          {error}
+    <div className="d-flex justify-content-center align-items-center mx-auto">
+      <div>
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn btn-primary "
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            onClick={() => {
+              navigateLink();
+            }}
+          >
+            Login
+          </button>
+
+          <div
+            className="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <form className="container mt-5" onSubmit={handlelogin}>
+                    <div className="mb-3">
+                      <label for="exampleInputEmail1" className="form-label">
+                        Email address
+                      </label>
+                      <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        type="email"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                      />
+                      <div id="emailHelp" className="form-text">
+                        {error}
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label for="exampleInputPassword1" className="form-label">
+                        Password
+                      </label>
+                      <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        type="password"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                      />
+                    </div>
+                    <div className="mb-3 form-check">
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="exampleCheck1"
+                      />
+                      <label className="form-check-label" for="exampleCheck1">
+                        Check me out
+                      </label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                      Login
+                    </button>
+                    <Link to="/signup">Create Account</Link>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-center align-items-center mt-3">
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal2"
+          >
+            Use Anonymously
+          </button>
+
+          <div
+            className="modal fade"
+            id="exampleModal2"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel2">
+                    Age Check
+                  </h5>
+                  {/* <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button> */}
+                </div>
+                <div className="modal-body2">
+                  {!ageVerified ? (
+                    <div className="d-flex justify-content-center align-items-center">
+                      <p>
+                        You must be over 18 years old to use this site. Please
+                        verify your age.
+                      </p>
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={() => {
+                          setAgeVerified(true);
+                          navigateLink();
+                        }}
+                      >
+                        I am over 18 years old
+                      </button>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <form>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button type="button" className="btn btn-primary">
+                      Save changes
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Password
-        </label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Login
-      </button>
-      <Link to="/signup">Create Account</Link>
-    </form>
+    </div>
   );
 };
 
