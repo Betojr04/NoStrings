476785e123743b84abd82e82219f10ad44f24658
michbalkany <askmitchb@gmail.com>
2023-02-27 22:23:27 +0000

    chat box now has the functionality to edit and delete a message

--- src/front/js/component/chatBox.js
+++ src/front/js/component/chatBox.js
@@ -13,9 +13,34 @@ function ChatBox() {
     event.preventDefault();
 
     if (inputValue.trim() !== "") {
-      setMessages([...messages, { text: inputValue }]);
+      setMessages([...messages, { text: inputValue, isEditable: false }]);
       setInputValue("");
     }
+  };
+
+  const handleEdit = (index) => {
+    const newMessages = [...messages];
+    newMessages[index].isEditable = true;
+    setMessages(newMessages);
+  };
+
+  const handleSave = (index, newText) => {
+    const newMessages = [...messages];
+    newMessages[index].text = newText;
+    newMessages[index].isEditable = false;
+    setMessages(newMessages);
+  };
+
+  const handleCancel = (index) => {
+    const newMessages = [...messages];
+    newMessages[index].isEditable = false;
+    setMessages(newMessages);
+  };
+
+  const handleDelete = (index) => {
+    const newMessages = [...messages];
+    newMessages.splice(index, 1);
+    setMessages(newMessages);
   };
 
   return (
@@ -23,7 +48,56 @@ function ChatBox() {
       <ul className="chat-box__messages">
         {messages.map((message, index) => (
           <li key={index} className="chat-box__message">
-            {message.text}
+            {message.isEditable ? (
+              <>
+                <input
+                  type="text"
+                  value={message.text}
+                  onChange={(event) => {
+                    const newMessages = [...messages];
+                    newMessages[index].text = event.target.value;
+                    setMessages(newMessages);
+                  }}
+                  className="chat-box__edit-input"
+                />
+                <div className="chat-box__edit-buttons">
+                  <button
+                    type="button"
+                    onClick={() => handleSave(index, message.text)}
+                    className="chat-box__button chat-box__button--save"
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleCancel(index)}
+                    className="chat-box__button chat-box__button--cancel"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                {message.text}
+                <div className="chat-box__buttons">
+                  <button
+                    type="button"
+                    onClick={() => handleEdit(index)}
+                    className="chat-box__button chat-box__button--edit"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleDelete(index)}
+                    className="chat-box__button chat-box__button--delete"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </>
+            )}
           </li>
         ))}
       </ul>
@@ -35,7 +109,10 @@ function ChatBox() {
           className="chat-box__input"
           placeholder="Type a message..."
         />
-        <button type="submit" className="chat-box__button">
+        <button
+          type="submit"
+          className="chat-box__button chat-box__button--send"
+        >
           Send
         </button>
       </form>
