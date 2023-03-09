ed43b691b39d1a166ad5df8f1b70780ca9d0a09b
michbalkany <askmitchb@gmail.com>
2023-03-09 02:00:49 +0000

    fixed profilemodal.js so that when anon user logs in profile says anon profile, vice versa for registered profile

--- src/front/js/component/profileModal.js
+++ src/front/js/component/profileModal.js
@@ -2,9 +2,9 @@ import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 
 const ProfileModal = ({ user }) => {
-  let isAnonymous = localStorage.getItem("isAnonymous");
-
-  return (
+  let isAnonymous = JSON.parse(localStorage.getItem("isAnonymous"));
+  console.log("is anonymous", isAnonymous);
+  return !isAnonymous ? (
     <div
       // className="modal fade"
       // id="staticBackdrop"
@@ -22,7 +22,60 @@ const ProfileModal = ({ user }) => {
         <div className="modal-content">
           <div className="modal-header">
             <h1 className="modal-title fs-5" id="staticBackdropLabel">
-              Modal title
+              Registered Profile
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="profile-container">
+              <img
+                className="col-12"
+                src="https://images.unsplash.com/photo-1503235930437-8c6293ba41f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZSUyMHBpeHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              />
+
+              <h3>{user?.full_name}</h3>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div
+      // className="modal fade"
+      // id="staticBackdrop"
+      // data-bs-backdrop="static"
+      // data-bs-keyboard="false"
+      // tabindex="-1"
+      // aria-labelledby="staticBackdropLabel"
+      // aria-hidden="true"
+      id="staticBackdrop"
+      className="modal hide fade in"
+      data-keyboard="false"
+      data-backdrop="static"
+    >
+      {console.log("is anon from anonProfile", isAnonymous)}
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="staticBackdropLabel">
+              Anonymous Profile
             </h1>
             <button
               type="button"
