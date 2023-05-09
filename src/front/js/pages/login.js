2c1095d414a594f0bd618e76f7d57afb660134fb
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-09 22:12:46 +0000

    added the new code for the entire login page taking into consideration the new modular components that were added.

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate, Link } from "react-router-dom";
+import LoginForm from "../component/LoginForm";
+import AnonLogin from "../component/AnonLogin";
 import GenderSelector from "../component/GenderSelector";
 
 const Login = () => {
@@ -196,6 +198,57 @@ const Login = () => {
         </div>
       </div>
     </div>
+
+    // IMPORTANT !!!this below code is the new login page that is more modular and using props from the components of loginform and anonlogin so that it is all seperate and not a huge all in one page of code.
+    //     import React, { useState, useEffect, useContext } from "react";
+    // import { useNavigate, Link } from "react-router-dom";
+    // import { Context } from "../store/appContext";
+    // import LoginForm from "./LoginForm";
+    // import AnonLogin from "./AnonLogin";
+
+    // const Login = () => {
+    //   const [isLoggedIn, setIsLoggedIn] = useState(false);
+    //   const [email, setEmail] = useState();
+    //   const [password, setPassword] = useState();
+    //   const [error, setError] = useState("");
+    //   const [ageVerified, setAgeVerified] = useState(false);
+    //   const [genderVerified, setGenderVerified] = useState(false);
+
+    //   let navigate = useNavigate();
+    //   const { store, actions } = useContext(Context);
+
+    //   useEffect(() => {
+    //     if (isLoggedIn) {
+    //       navigate("/home");
+    //     }
+    //   }, [isLoggedIn]);
+
+    //   return (
+    //     <div className="d-flex justify-content-center align-items-center mx-auto">
+    //       <div>
+    //         <LoginForm
+    //           email={email}
+    //           setEmail={setEmail}
+    //           password={password}
+    //           setPassword={setPassword}
+    //           error={error}
+    //           setError={setError}
+    //           setIsLoggedIn={setIsLoggedIn}
+    //           actions={actions}
+    //         />
+    //         <AnonLogin
+    //           ageVerified={ageVerified}
+    //           setAgeVerified={setAgeVerified}
+    //           genderVerified={genderVerified}
+    //           setGenderVerified={setGenderVerified}
+    //           actions={actions}
+    //         />
+    //       </div>
+    //     </div>
+    //   );
+    // };
+
+    // export default Login;
   );
 };
 
